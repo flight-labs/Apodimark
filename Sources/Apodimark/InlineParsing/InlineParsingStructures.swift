dd5ec75ef994d117b49c70c0a1081876190d2b5d
Paul Betts <paul@paulbetts.org>
2017-11-10 14:24:18 -0800

    Let's just remove the underscore spaces code

--- Sources/Apodimark/InlineParsing/InlineParsingStructures.swift
+++ Sources/Apodimark/InlineParsing/InlineParsingStructures.swift
@@ -58,13 +58,6 @@ struct DelimiterState: OptionSet {
         case Codec.tilde, Codec.asterisk, Codec.underscore:
             if isRightFlanking { state.formUnion(.closing) }
             if isLeftFlanking  { state.formUnion(.opening) }
-
-        /*
-        case Codec.underscore:
-            if isRightFlanking && (!isLeftFlanking  || prev == .punctuation) { state.formUnion(.closing) }
-            if isLeftFlanking  && (!isRightFlanking || next == .punctuation) { state.formUnion(.opening) }
-        */
-
         default:
             fatalError()
         }
