51ce11444833471450965dc8a5f236e0f278f4fb
Max Desiatov <max@desiatov.com>
2019-01-20 19:17:44 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,31 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to
+// build this package.
 
 import PackageDescription
 
-let package = Package(name: "Apodimark")
+let package = Package(
+  name: "Apodimark",
+  products: [
+    // Products define the executables and libraries produced by a package, and
+    // make them visible to other packages.
+    .library(
+      name: "Apodimark",
+      targets: ["Apodimark"]
+    ),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define
+    // a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in
+    // packages which this package depends on.
+    .target(
+      name: "Apodimark",
+      dependencies: []
+    ),
+  ]
+)
+
